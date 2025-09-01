2cb376ac3d8d38b5732a6b9b90cf21169396ea88
ProgrammingLama <jamilyaz@protonmail.com>
2025-09-01 20:40:10 +0200

    remodel measurement and region

--- models/Measurement.cs
+++ models/Measurement.cs
@@ -5,25 +5,28 @@ public class Measurement
     private float windSpeed;
     private float airTemperature;
     private float precipitation;
-    private float waterLevel;
+
+    private float pressure;
     private DateTime timestamp;
     private Region region;
     
-
-    public float WaterLevel => this.waterLevel;
     public float WindSpeed => this.windSpeed;
     public float AirTemperature => this.airTemperature;
     public float Precipitation => this.precipitation;
+
+    public float Pressure => this.pressure;
     public DateTime Timestamp => this.timestamp;
     public Region Region => this.region;
 
-    public Measurement(float windSpeed, float airTemperature, float precipitation, float waterLevel, DateTime timestamp, Region region)
+
+    public Measurement(float windSpeed, float airTemperature, float precipitation, float pressure, DateTime timestamp, Region region)
     {
         this.timestamp = timestamp;
         this.region = region;
+        this.pressure = pressure;
         this.windSpeed = windSpeed;
         this.airTemperature = airTemperature;
         this.precipitation = precipitation;
-        this.waterLevel = waterLevel;  
+
     }
 }
--- models/Region.cs
+++ models/Region.cs
@@ -2,89 +2,61 @@
 
 public class Region
 {
-    private string _continent { get; set; }
-    private string _country {get; set;}
+    private string _country { get; set; }
     private string _city { get; set; }
+    private string _region { get; set; }
+    private double _longitude { get; set; }
+    private double _latitude { get; set; }
 
-    private float _floodRiskLevel;
-    private float _temperatureRiskLevel;
-    private float _windRiskSpeed;
-    private float _precipitationRiskLevel;
-    
 
-    public float floodRiskLevel
+    public string region
     {
-        get { return _floodRiskLevel; }
-        set { _floodRiskLevel = value; }
-    }
-
-    public float temperatureRiskLevel
-    {
-        get { return _temperatureRiskLevel; }
-        set => _temperatureRiskLevel = value;
-    }
-
-    public float windRiskSpeed
-    {
-        get { return _windRiskSpeed; }
-        set => _windRiskSpeed = value;
-    }
-
-    public float precipitationRiskLevel
-    {
-        get { return _precipitationRiskLevel; }
-        set => _precipitationRiskLevel = value;
-    }
-    public string continent
-    {
-        get { return _continent; }
-        set { _continent = value; }
+        get { return _country; }
+        set { _country = value; }        
     }
     public string country
     {
         get { return _country; }
         set { _country = value; }
     }
-    
+
     public string city
     {
         get { return _city; }
         set { _city = value; }
     }
 
-
-    public Region(string continent, string country, string city, float floodRiskLevel, float temperatureRiskLevel, float windRiskSpeed, float precipitationRiskLevel)
+    public double longitude
     {
-        this._continent = continent;
+        get { return _longitude; }
+        set { longitude = value; }
+    }
+    public double latitude
+    {
+        get { return _latitude; }
+        set { latitude = value; }
+    }
+
+    public Region(string country, string region, string city, double longitude, double latitude)
+    {
+
         this._country = country;
         this._city = city;
-        this._floodRiskLevel = floodRiskLevel;
-        this._temperatureRiskLevel = temperatureRiskLevel;
-        this._windRiskSpeed = windRiskSpeed;
-        this._precipitationRiskLevel = precipitationRiskLevel;
+        this.region = region;
+        this.longitude = longitude;
+        this.latitude = latitude;
+
+
     }
-    
+
     public string GetRegionInfo()
     {
         return
             $"Following Inforation about Region" +
-            $"Continent: {_continent}" +
             $"Country: {_country}" +
-            $"City: {_city}";
-    }
-    
-    public string GetRiskLevels()
-    {
-        return
-            $"Following Inforation about Risk Levels" +
-            $"Flood Risk Level: {_floodRiskLevel}" +
-            $"Temperature Risk Level: {_temperatureRiskLevel}" +
-            $"Wind Risk Level: {_windRiskSpeed}" +
-            $"Precipitation Risk Level: {_precipitationRiskLevel}";
-    }
+            $"Region: {region}" +
+            $"City: {_city}" +
+            $"Coordinates: {_latitude}, {_longitude}";
 
-    public override bool Equals(object? obj)
-    {
-        return this._city == (obj as Region).city;
     }
 }
